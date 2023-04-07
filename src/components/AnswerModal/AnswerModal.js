26cf725b5ecfc384bd1c17e80286fea60b9c7231
Kiryl Fedaruk <elhora.edu@gmail.com>
2023-04-08 00:03:53 +0300

    feature: add random quests

--- src/components/AnswerModal/AnswerModal.js
+++ src/components/AnswerModal/AnswerModal.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './AnswerModal.css';
-import data from "../../assets/data/steps.json";
+// import data from "../../assets/data/steps.json";
+
 
 
 function AnswerModal(props) {
-    let answer = data['ex'][props.question]["answer"]
-    let answerCode = data['ex'][props.question]["answerCode"]
 
+    let answer = props.answer
+    let answerCode = props.answerCode
     return (
         <>
             <div className='answer-modal'>
-                <p className='answer-text'>{answer}</p>
+                <p className='answer-text'>{answer ? answer : ''}</p>
                 
                 {answerCode && 
                 <pre className="language-javascript">
--- src/components/contentBox/Content.js
+++ src/components/contentBox/Content.js
@@ -4,35 +4,53 @@ import AnswerModal from '../AnswerModal/AnswerModal';
 import data from "../../assets/data/steps.json";
 import { useState } from 'react';
 
+let localCopyData = JSON.parse(JSON.stringify(data))
+let randomQuestion = randInt(0, localCopyData['ex'].length - 1)
+console.log(randomQuestion)
+
+function randInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 function Content() {
-    const [ex, setEx] = useState(0)
+    const [ex, setEx] = useState(randomQuestion)
     const [isVisible, setIsVisible] = useState(false);
 
-    let title = (data["ex"].length > ex) ? data["ex"][ex]['title'] : "Null"
-    let description = (data["ex"].length > ex) ? data["ex"][ex]['description'] : "Null"
+    let questionAmount = localCopyData['ex'].length
+    let title = localCopyData["ex"][ex]['title']
+    let description = localCopyData["ex"][ex]['description']
+    let answer = localCopyData["ex"][ex]['answer']
+    let answerCode = localCopyData["ex"][ex]['answerCode']
+
 
     const handleClick = () => {
         setIsVisible(!isVisible);
     }
 
-    function changeEx() {
-        setEx(randInt(0, data['ex'].length - 1))
-    }
 
-    function randInt(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
+    questionAmount = localCopyData['ex'].length
+    console.log("вопросов:", questionAmount)
+    console.log("текущий", ex)
+    console.log("список", localCopyData['ex'])
+    function changeEx() {
+        localCopyData['ex'].splice(randomQuestion, 1)
+        randomQuestion = randInt(0, localCopyData['ex'].length - 1)
+        setEx(randomQuestion)
+
     }
 
     return (
         <>
             <div className='content-area'>
-                <h1 className='step-title'>{title}</h1>
-                <p className='step-description'>{description}</p>
+                <h1 className='step-title'>{(questionAmount > 0) ? title : "Конец"}</h1>
+                <p className='step-description'>{(questionAmount > 0) ? description : 'Вопросы закончились'}</p>
                 <div>
                     <button onClick={handleClick} className='content-btn show-answer'>Ответ</button>
-                    <button onClick={changeEx} className='content-btn next-step'>Следующий</button>
+                    <button onClick={changeEx} className='content-btn next-step'>Дальше</button>
                 </div>
-                {isVisible && <AnswerModal question={ex}/>}
+                {isVisible && <AnswerModal
+                    question={ex}
+                    answer={answer}
+                    answerCode={answerCode} />}
             </div>
         </>
     )
